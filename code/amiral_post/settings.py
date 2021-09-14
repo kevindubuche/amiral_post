fe2bf96e889031e184d67bfe24774c31c4437e19
Kevin Dubuche <kevindubuche@gmail.com>
2021-09-14 17:04:39 -0400

    -

--- code/amiral_post/settings.py
+++ code/amiral_post/settings.py
@@ -79,7 +79,8 @@ WSGI_APPLICATION = 'amiral_post.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
