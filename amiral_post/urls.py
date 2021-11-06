5f99b73f115facdeed79a072355339351bf0e36a
Akuma <jeanothniel36@gmail.com>
2021-11-06 10:21:09 +0100

    photodis

--- amiral_post/urls.py
+++ amiral_post/urls.py
@@ -32,6 +32,6 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
     path('admin/', admin.site.urls),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^public/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
